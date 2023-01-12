6241527768aa0871588f54aeacb5c19041d1be2d
Pobitrodeb <pobitrodn@gmail.com>
2023-01-13 00:21:14 +0600

    Install Socialite and Cofigure session successfully done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,13 +41,10 @@
 
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                <p>Login With : </p>
+                               <a href="" class="btn btn-success"> Facebook  </a>
+                               <a href="" class="btn btn-success"> Google  </a>
+                               <a href="" class="btn btn-success"> GitHub  </a>
                             </div>
                         </div>
 
